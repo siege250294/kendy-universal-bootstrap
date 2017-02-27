d23c34609670e29ea4e0b333fd4eb83d1485821f
Nhan Hoang <siege250294@gmail.com>
2017-02-27 12:09:27 +0700

    gulp: add environment config when run gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp')
 const gutil = require('gulp-util')
+const genv = require('gulp-env')
+const chalk = require('chalk')
 const del = require('del')
 const { resolve } = require('path')
 const webpack = require('webpack')
@@ -10,25 +12,40 @@ gulp.task('clean:public', (cb) => {
 	})
 })
 
+gulp.task('set-environment-production', () => {
+	genv({
+		vars: {
+			NODE_ENV: 'production'
+		}
+	})
+})
+gulp.task('set-environment-development', () => {
+	genv({
+		vars: {
+			NODE_ENV: 'development'
+		}
+	})
+})
 
-const clientConfig = Object.create(require('./webpack/config.client'))
-gulp.task('build:client', (cb) => {
-	process.env.NODE_ENV = 'production'
+gulp.task('build:client', ['clean:public', 'set-environment-production'], (cb) => {
+	const clientConfig = Object.create(require('./webpack/config.client'))
+	gutil.log('[is-development]', chalk.red(clientConfig.cache))
 	webpack(clientConfig, (err, stats) => {
 		if (err) {
 			throw new gutil.PluginError('[webpack-client]', err)
 		}
-		gutil.log(stats.toString({ colors: true }))
+		gutil.log('[webpack-client]', stats.toString({ colors: true }))
 	})
 })
 
-const serverConfig = Object.create(require('./webpack/config.server'))
+
 gulp.task('build:server', (cb) => {
-	process.env.NODE_ENV = 'production'
+	const serverConfig = Object.create(require('./webpack/config.server'))
+	gutil.log('[is-development]', serverConfig.cache)
 	webpack(serverConfig, (err, stats) => {
 		if (err) {
 			throw new gutil.PluginError('[webpack-server]', err)
 		}
-		gutil.log(stats.toString({ colors: true }))
+		gutil.log('[webpack-server]', stats.toString({ colors: true }))
 	})
 })
