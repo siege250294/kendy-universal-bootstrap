e176c3ac6992ebe99e3d8208f974b8d206007c8e
Nhan Hoang <siege250294@gmail.com>
2017-03-07 23:19:05 +0700

    src: move server-side render to production state

--- src/server/dev-server.js
+++ src/server/dev-server.js
@@ -4,14 +4,6 @@ import webpackDevMiddleware from 'webpack-dev-middleware'
 import webpackHotMiddleware from 'webpack-hot-middleware'
 import webpackClientConfig from '../../webpack/config.client'
 import { resolve } from 'path'
-import React from 'react'
-import { renderToString } from 'react-dom/server'
-import { match, RouterContext } from 'react-router'
-import routes from '../routes'
-import Root from './'
-import { Provider } from 'react-redux'
-import configureStore from '../store/configureStore'
-import Immutable from 'immutable'
 
 const app = express()
 const bundle = webpack(Object.create(webpackClientConfig))
@@ -26,39 +18,9 @@ app.use(webpackDevMiddleware(bundle, {
 	}
 }))
 
-function renderPage(appHtml) {
-	return `
-		<!doctype html public="storage">
-		<html>
-		<meta charset="utf-8"/>
-		<title>Kendy React Universal Starter Kit</title>
-		<div id="app">${appHtml}</div>
-		<script src="/bundle.js"></script>
-	`
-}
-
-// Configurate store by using an initial state
-const store = configureStore({
-	products: Immutable.List([
-		{category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
-		{category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
-		{category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
-		{category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
-		{category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
-		{category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
-	])
-})
-
 // Then using the index.html for all routes - client-side render
 app.get('*', function(req, res) {
-	match( { routes: routes, location: req.url }, (err, redirect, props) => {
-		const appHtml = renderToString((
-			<Provider store={store}>
-				<RouterContext { ...props} />
-			</Provider>
-		))
-		res.send(renderPage(appHtml))
-	})
+	res.sendFile(resolve(__dirname, '../index.html'))
 })
 
 export default app
